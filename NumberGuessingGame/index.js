fdb7e26412843ae12a9d9dcc168d108f8c14bb24
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-03-29 23:38:27 +0500

    NumberGuessingGame Added

---
+++ NumberGuessingGame/index.js
@@ -0,0 +1,31 @@
+#!  /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+let randomNumberr = [];
+let userAnswerr = [];
+const game = async () => {
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    const userAnswer = await inquirer.prompt([
+        {
+            name: "userGuess",
+            type: "input",
+            message: "Enter Your Guessed Number (1-10): "
+        }
+    ]);
+    const guess = parseInt(userAnswer.userGuess);
+    userAnswerr.push(guess);
+    randomNumberr.push(randomNumber);
+    if (guess === randomNumber) {
+        console.log(chalk.whiteBright("CONGRATULATIONS!!! YOU GOT IT RIGHT!!!"));
+    }
+    else {
+        console.log(chalk.red("AWW!!! YOU GOT IT WRONG!!!"));
+        console.log(`The number was ${randomNumber}`);
+        console.log(chalk.green("!!!THE GAME IS RESTARTING FROM THE BEGINNING!!!"));
+        randomNumberr = [];
+        userAnswerr = [];
+        await game();
+    }
+};
+await game();
+export default game;
